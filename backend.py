d3879ad8a9c56e5a6b1ac9a46e02c400133a44ff
omar ahmed <omarzaafan732@gmail.com>
2024-05-17 06:57:20 +0300

    make some changes

--- backend.py
+++ backend.py
@@ -27,26 +27,11 @@ def product_catalog():
         for row in cursor.fetchall()
     ]
     if products is not None:
-        return render_template('index.html', products=products)
+        return render_template('Product.html', products=products)
 
 @app.route("/cart_management", methods=['GET'])
 def cart_management():    
-
-    return render_template('frontend.html', data=all_data)
-    '''products = []
-    conn = db_connection()
-    cursor = conn.cursor()
-    for i in all_data:
-        print[i]
-        cursor.execute("SELECT * FROM product WHERE id = ?", (i,))
-        row = cursor.fetchone()
-        if row:
-            products.append(dict(id=row[0], name=row[1], price=row[2], image=base64.b64encode(row[3]).decode('utf-8') if isinstance(row[3], bytes) else None, quantity=row[4]))
-    
-    conn.close()
-    
-    return render_template('frontend.html', products=products)'''
-    '''if data:
+    if data:
         products = []
         conn = db_connection()
         cursor = conn.cursor()
@@ -62,7 +47,8 @@ def cart_management():
         else:
             return jsonify({"error": "No products found"}), 404
     else:
-        return jsonify({"error": "No data provided"}), 400'''
+        return jsonify({"error": "No data provided"}), 400
+    return render_template('frontend.html', products=products)
     
 @app.route("/cart_management", methods=['POST'])
 def cart_management_POST():
@@ -79,25 +65,7 @@ def data():
         data = list(request.form.values())
         #jso = json.loads(jsonify(data))
         
-        all_data.append(data)    
-        #return redirect(url_for("cart_management", data=data))
-        '''if all_data:
-            products = []
-            conn = db_connection()
-            cursor = conn.cursor()
-            for i in all_data:
-                cursor.execute("SELECT * FROM product WHERE id = ?", (i[0],))
-                row = cursor.fetchone()
-                if row:
-                    products.append(dict(id=row[0], name=row[1], price=row[2], image=base64.b64encode(row[3]).decode('utf-8') if isinstance(row[3], bytes) else None, quantity=row[4]))
-            
-            conn.close()
-            if products:
-                return render_template('frontend.html', products=products)
-            else:
-                return jsonify({"error": "No products found"}), 404
-        else:
-            return jsonify({"error": "No data provided"}), 400'''
+        all_data.append(data)
     
 
 
